87f0593b5fb79df5df1ed5d190f7e43147a727b3
Jay Harris <jay.harris@outlook.co.nz>
2019-02-06 20:19:58 +1100

    Hacky layout

--- src/ArticlesList.tsx
+++ src/ArticlesList.tsx
@@ -4,9 +4,11 @@ import { fakeArticle } from "./faking/fakeArticle";
 import { Article } from "./model/article";
 
 export default (props) => {
-    const articles: Article[] = [1,2,3,4,5].map(i => fakeArticle());
+    const articles: Article[] = [1, 2, 3, 4, 5].map(i => fakeArticle());
 
-    return <div>
-        {articles.map((a, i) => <ArticleCard article={a} key={a.id || i}/>)}
+    return <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-evenly' }}>
+        {articles.map((a, i) => <div style={{ flexBasis: '40%', margin: '5px' }}>
+            <ArticleCard article={a} key={a.id || i} />
+        </div>)}
     </div>
 }
