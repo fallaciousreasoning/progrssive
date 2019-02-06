0b8c0b953a792e455ebf8bfbeaedde811e4228d0
Jay Harris <jay.harris@outlook.co.nz>
2019-02-06 19:16:51 +1100

    Adds appbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
+import { IconButton, Toolbar, Typography } from '@material-ui/core';
+import AppBar from '@material-ui/core/AppBar';
+import MenuIcon from '@material-ui/icons/Menu';
 import React, { Component } from 'react';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    return <div>
+      <AppBar position="absolute" color="primary">
+        <Toolbar>
+          <IconButton>
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="title">Progrssive Reader</Typography>
+        </Toolbar>
+      </AppBar>
+    </div>;
   }
 }
 
