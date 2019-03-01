a195c01114edae00275990fd6da6e26810050b68
Jay Harris <jay.harris@outlook.co.nz>
2019-03-01 17:04:24 +1100

    Upgrades react and adds a margin to article list

--- src/EntryList.tsx
+++ src/EntryList.tsx
@@ -1,14 +1,24 @@
 import { Grid } from '@material-ui/core';
+import { makeStyles } from '@material-ui/styles'
 import React from 'react';
 import Entry from "./Entry";
 import { stream } from './faking/fakeStream';
 
-export default (props) => {
-    const entries = stream.items;
+const useStyles = makeStyles({
+  root: {
+    margin: '10px'
+  }
+});
 
-    return <Grid spacing={24} container justify='center' wrap='wrap'>
-        {entries.map(e => <Grid item key={e.id} lg={3} md={6} sm={12}>
-          <Entry entry={e}/>
-        </Grid>)}
+export default (props) => {
+  const classes = useStyles();
+  const entries = stream.items;
+
+  return <div className={classes.root}>
+    <Grid spacing={24} container justify='center' wrap='wrap'>
+      {entries.map(e => <Grid item key={e.id} lg={3} md={6} sm={12}>
+        <Entry entry={e} />
+      </Grid>)}
     </Grid>
+  </div>
 }
