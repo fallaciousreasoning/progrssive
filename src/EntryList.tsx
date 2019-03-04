137adb2a3c41c92610154719100e3774350284c5
Jay Harris <jay.harris@outlook.co.nz>
2019-03-04 19:27:05 +1100

    Adds a loader

--- src/EntryList.tsx
+++ src/EntryList.tsx
@@ -1,8 +1,8 @@
-import { Grid } from '@material-ui/core';
-import { makeStyles } from '@material-ui/styles'
+import { CircularProgress, Grid } from '@material-ui/core';
+import { makeStyles } from '@material-ui/styles';
 import React from 'react';
 import Entry from "./Entry";
-import { stream } from './faking/fakeStream';
+import { Stream } from './model/stream';
 
 const useStyles = makeStyles({
   root: {
@@ -10,9 +10,12 @@ const useStyles = makeStyles({
   }
 });
 
-export default (props) => {
+export default (props: { stream: Stream }) => {
   const classes = useStyles();
-  const entries = props.stream ? props.stream.items : stream.items;
+  const entries = props.stream && props.stream.items;
+
+  if (!entries)
+    return <CircularProgress/>;
 
   return <div className={classes.root}>
     <Grid spacing={24} container justify='center' wrap='wrap'>
