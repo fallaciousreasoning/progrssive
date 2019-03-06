81c0c5ad2b24bb56dacaf3b9a090c9e2e869e680
Jay Harris <jay.harris@outlook.co.nz>
2019-03-06 21:00:38 +1100

    Adds a redirect for testing

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import AppBar from '@material-ui/core/AppBar';
 import MenuIcon from '@material-ui/icons/Menu';
 import { makeStyles, ThemeProvider } from '@material-ui/styles';
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import { AnimatedRoute } from 'react-router-transition';
+import { BrowserRouter, Redirect, Route } from 'react-router-dom';
+import { AnimatedSwitch } from 'react-router-transition';
 import AppDrawer from './AppDrawer';
 import EntryViewer from './EntryViewer';
 import StreamViewer from './StreamViewer';
@@ -34,8 +34,12 @@ export const App = (props) => {
         </Toolbar>
       </AppBar>
       <div className={styles.root}>
-          <AnimatedRoute path='/stream/:streamId*' component={StreamViewer}  atEnter={slideTransition.atEnter} atLeave={slideTransition.atLeave} atActive={slideTransition.atActive} mapStyles={mapStyles} />
-          <AnimatedRoute path='/entries/:entryId*' component={EntryViewer}  atEnter={slideTransition.atEnter} atLeave={slideTransition.atLeave} atActive={slideTransition.atActive} mapStyles={mapStyles}/>
+        <AnimatedSwitch atEnter={slideTransition.atEnter} atLeave={slideTransition.atLeave} atActive={slideTransition.atActive} mapStyles={mapStyles}>
+          <Route path='/stream/:streamId*' component={StreamViewer} />
+          <Route path='/entries/:entryId*' component={EntryViewer} />
+          {/* TODO: Remove hacky redirect for testing */}
+          <Redirect to='/stream/user/e8ca5f09-ffa1-43d8-9f28-867ed8ad876a/category/global.all'/>
+        </AnimatedSwitch>
       </div>
     </ThemeProvider>
   </BrowserRouter>;
