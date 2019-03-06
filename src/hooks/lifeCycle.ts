ae1f013c17555dba102facb2e316927937e36092
Jay Harris <jay.harris@outlook.co.nz>
2019-03-06 23:01:03 +1100

    Fixes the onMount hook

--- src/hooks/lifeCycle.ts
+++ src/hooks/lifeCycle.ts
@@ -1,17 +1,9 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 
 type Return = void | (() => any | void);
 
 export const useOnMount = (onMount: () => Return) => {
-    const [mounted, setMounted] = useState(false);
-    const [onUnmount, setOnUnmount] = useState(undefined);
-
     useEffect(() => {
-        if (mounted) return onUnmount;
-
-        const result = onMount();
-        setMounted(true);
-        setOnUnmount(result);
-        return result;
-    });
+        return onMount();
+    }, []);
 }
