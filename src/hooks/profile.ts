d0fd2297cf4c851907135f78deecb46466e6c41d
Jay Harris <jay.harris@outlook.co.nz>
2019-03-09 14:46:19 +1100

    only fetches profile once

--- src/hooks/profile.ts
+++ src/hooks/profile.ts
@@ -3,11 +3,20 @@ import { useStore } from "./store";
 import { executeOnce } from "./promise";
 import { getProfile } from "../api/profile";
 
+let fetching = false;
+
 export const useProfile = (): Profile => {
     const store = useStore();
 
     // If we haven't cached the profile, get it from the internet.
-    executeOnce(() => !store.profile && getProfile().then(profile => store.profile = profile));
+    executeOnce(() => {
+        if (fetching) return;
+        fetching = true;
+
+        return !store.profile && getProfile()
+            .then(profile => store.profile = profile)
+            .then(() => fetching = false);
+    });
 
     return store.profile;
 }
