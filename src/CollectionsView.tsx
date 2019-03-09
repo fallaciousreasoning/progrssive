532f746b7185c7e9d7750292912ad75828b69c06
Jay Harris <jay.harris@outlook.co.nz>
2019-03-09 15:19:03 +1100

    Adds a link to all categories

--- src/CollectionsView.tsx
+++ src/CollectionsView.tsx
@@ -1,10 +1,26 @@
 import * as React from 'react';
 import CollectionView from './CollectionView';
 import { useCollections } from './hooks/collections';
+import { ListItem, ListItemText, ListItemIcon } from '@material-ui/core';
+import { RssFeed } from '@material-ui/icons';
+import { withRouter } from 'react-router';
+import { useProfile } from './hooks/profile';
+import { getAllId } from './api/streams';
+
+const AllEntry = withRouter(props => {
+    const profile = useProfile();
+    return <ListItem onClick={() => props.history.push(`/stream/${getAllId(profile.id)}`)} button>
+        <ListItemIcon>
+            <RssFeed />
+        </ListItemIcon>
+        <ListItemText>All</ListItemText>
+    </ListItem>
+});
 
 export default (props) => {
     const categories = useCollections();
     return <>
-        {categories.map(c => <CollectionView collection={c} key={c.id}/>)}
+        <AllEntry />
+        {categories.map(c => <CollectionView collection={c} key={c.id} />)}
     </>;
 }
