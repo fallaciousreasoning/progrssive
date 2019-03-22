a44df8f53520b11873863d6436b92c09cd78c5e8
Jay Harris <jay.harris@outlook.co.nz>
2019-03-22 19:37:09 +1100

    Fixes bug in entry fetching

--- src/api/entry.ts
+++ src/api/entry.ts
@@ -2,6 +2,6 @@ import { makeRequest } from "./common";
 import { Entry } from "../model/entry";
 
 export const getEntry = async (entryId: string) => {
-    const entries = await makeRequest<Entry[]>(`/entries/${entryId}`);
+    const entries = await makeRequest<Entry[]>(`/entries/${encodeURIComponent(entryId)}`);
     return entries[0];
 }
