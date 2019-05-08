f0b5e7c6b5cc2ace57cf90dd751ef6180f54d521
Jay Harris <jay.harris@outlook.co.nz>
2019-05-08 20:24:24 +1000

    Begins work on the settings page

--- src/SettingsPage.tsx
+++ src/SettingsPage.tsx
@@ -1,11 +1,57 @@
 import * as React from 'react';
-import { List, ListItem, ListItemText } from '@material-ui/core';
+import { List, ListItem, ListItemText, ListItemSecondaryAction, Switch, Divider } from '@material-ui/core';
+import { Slider } from '@material-ui/lab';
+import { makeStyles } from '@material-ui/styles';
+
+const useStyles = makeStyles({
+    slider: {
+        width: '48px !important'
+    }
+});
 
 export default (props) => {
+    const styles = useStyles();
+
     return <div>
         <List>
             <ListItem>
-                <ListItemText primary='foo' secondary='bar'/>
+                <ListItemText
+                    primary='Read opened articles'
+                    secondary='Marks articles as read when you open them.'/>
+                <ListItemSecondaryAction>
+                    <Switch/>
+                </ListItemSecondaryAction>
+            </ListItem>
+            <ListItem>
+                <ListItemText
+                    primary='Auto mark as read'
+                    secondary='Marks articles as read when you scoll past them.'/>
+                <ListItemSecondaryAction>
+                    <Switch/>
+                </ListItemSecondaryAction>
+            </ListItem>
+            <ListItem>
+                <ListItemText
+                    primary='Double tap to close articles'
+                    secondary='Whether articles can be closed by double tapping them.'/>
+                <ListItemSecondaryAction>
+                    <Switch/>
+                </ListItemSecondaryAction>
+            </ListItem>
+            <Divider/>
+            <ListItem>
+                <ListItemText
+                    primary="Article Text Size"
+                    secondary="Controls the size of the article text"/>
+                <ListItemSecondaryAction>
+                    <Slider
+                        className={styles.slider}
+                        min={1}
+                        max={5}
+                        step={1}
+                        onChange={console.log}
+                        value={3}/>
+                </ListItemSecondaryAction>
             </ListItem>
         </List>
     </div>;
