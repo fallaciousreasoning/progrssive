c7ec6c79a62aed702d91461c1ad685a898dae974
Jay Harris <jay.harris@outlook.co.nz>
2019-10-21 22:29:29 +1100

    Adds docs folder for Github Pages

---
+++ docs/precache-manifest.abf23ae84b6a464aa240b4d42e826fdf.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5092a8181a0ece6f7087",
+    "url": "/static/js/main.5092a818.chunk.js"
+  },
+  {
+    "revision": "cf5ffe051b2dc41f3473",
+    "url": "/static/js/1.cf5ffe05.chunk.js"
+  },
+  {
+    "revision": "60a12ce286a54b8bb56667112efa61dc",
+    "url": "/index.html"
+  }
+];
