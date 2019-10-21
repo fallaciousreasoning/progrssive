029be8d60785449534727984e32ba0804f522840
Jay Harris <jay.harris@outlook.co.nz>
2019-10-21 22:51:50 +1100

    Updates pages

---
+++ docs/build/precache-manifest.5e5adca7981cd9bbced6bdda6896686f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "065467d17459f9a6ad88",
+    "url": "/progrssive/static/js/runtime~main.065467d1.js"
+  },
+  {
+    "revision": "9b4558babbbcc98a0cd3",
+    "url": "/progrssive/static/js/main.9b4558ba.chunk.js"
+  },
+  {
+    "revision": "cf5ffe051b2dc41f3473",
+    "url": "/progrssive/static/js/1.cf5ffe05.chunk.js"
+  },
+  {
+    "revision": "2b0b004cc44e4f5e66e267bbd977c5fc",
+    "url": "/progrssive/index.html"
+  }
+];
