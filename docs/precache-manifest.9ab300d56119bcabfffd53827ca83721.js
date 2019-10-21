b1d0fbaa65e9fd817e725209147a3157d7f59c04
Jay Harris <jay.harris@outlook.co.nz>
2019-10-21 22:35:28 +1100

    Adds gh-pages

---
+++ docs/precache-manifest.9ab300d56119bcabfffd53827ca83721.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "065467d17459f9a6ad88",
+    "url": "/progrssive/static/js/runtime~main.065467d1.js"
+  },
+  {
+    "revision": "a8e7cc711b4873903c1a",
+    "url": "/progrssive/static/js/main.a8e7cc71.chunk.js"
+  },
+  {
+    "revision": "cf5ffe051b2dc41f3473",
+    "url": "/progrssive/static/js/1.cf5ffe05.chunk.js"
+  },
+  {
+    "revision": "198ee1f3f861c9101e2be7278caf3cbc",
+    "url": "/progrssive/index.html"
+  }
+];
