93c1ac27e66efd86b1e6fbec767f356763bc6b93
Jay Harris <jay.harris@outlook.co.nz>
2020-05-20 09:50:29 +1000

    Begins cleaning up useStyles

--- src/AppBar.tsx
+++ src/AppBar.tsx
@@ -1,19 +1,18 @@
 import React, { useState, useEffect } from 'react';
-import { Toolbar, AppBar, IconButton, Typography } from '@material-ui/core';
+import { Toolbar, AppBar, IconButton, Typography, makeStyles } from '@material-ui/core';
 import { Menu } from '@material-ui/icons';
 import AppDrawer from './AppDrawer';
-import { makeStyles } from '@material-ui/styles';
 
 export const AppBarContext: React.Context<React.Ref<any>> = React.createContext(undefined);
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
     pageActions: {
         flex: 1,
         display: 'flex',
-        flexDirection: 'row-reverse',
+        flexDirection: 'row-reverse' as any,
         width: '100%'
     }
-})
+}))
 
 export default (props) => {
     const styles = useStyles();
