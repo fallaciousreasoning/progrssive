6208f76e89d2c53d6e1705fa9be16fe09a1ec551
Jay Harris <jay.harris@outlook.co.nz>
2020-05-22 10:51:35 +1000

    Attempt to remove tracking pixels

--- src/services/entry.ts
+++ src/services/entry.ts
@@ -1,12 +1,25 @@
 import { Entry } from "../model/entry";
 import relativeDate from 'tiny-relative-date';
 
+export const sanitizeContent = (contentString: string) => {
+    if (!contentString)
+        return contentString;
+
+
+    // Remove tracking pixels.
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(`<div>${contentString}</div>`, "text/html");
+    // Remove tracking pixels.
+    doc.querySelectorAll("img[width='1']").forEach(i => i.remove());
+    contentString = doc.body.innerHTML;
+    return contentString;
+}
 export const getEntryContent = (entry: Entry) => {
     const detail = entry.content || entry.summary;
-    return detail && detail.content;
+    return sanitizeContent(detail && detail.content);
 }
 
-export const getEntrySummary = (entry: Entry) => entry.summary && entry.summary.content;
+export const getEntrySummary = (entry: Entry) => sanitizeContent(entry.summary && entry.summary.content);
 
 export const getEntryByline = (entry: Entry) => `${entry.engagement ? entry.engagement + ' ' : ''}${entry.origin && entry.origin.title} / ${relativeDate(new Date(entry.published))}`;
 
