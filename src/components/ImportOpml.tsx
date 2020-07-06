35fc72542b3c64f1577732af71a85943931d6d0b
Jay Harris <jay.harris@outlook.co.nz>
2020-07-06 13:24:34 +1200

    Flattens OPML structure

--- src/components/ImportOpml.tsx
+++ src/components/ImportOpml.tsx
@@ -3,17 +3,40 @@ import { Button, ButtonProps } from '@material-ui/core'
 import opmlToJson from 'opml-to-json';
 import { pickFile, getFileText } from '../utils/files';
 
+interface OpmlNode {
+    text: string;
+    title: string;
+    htmlurl: string;
+    folder: string;
+    xmlurl: string;
+    type: string;
+    children?: OpmlNode[];
+}
 interface Props {
 
 }
 
-export const parseOpml = (opml: string) => {
+export const parseOpml = (opml: string): Promise<OpmlNode[]> => {
+    const onlyFeeds = (root: OpmlNode, nodes: OpmlNode[] = []): OpmlNode[] => {
+        if (root.xmlurl){
+            nodes.push(root);
+        }
+
+        if (root.children) {
+            for (const child of root.children)
+                onlyFeeds(child, nodes);
+        }
+
+        return nodes;
+    }
     return new Promise((accept, reject) => {
         opmlToJson(opml, (error, json) => {
             if (error) {
                 reject(error);
             } else {
-                accept(json);
+                // Flatten the structure.
+                const feeds = onlyFeeds(json);
+                accept(feeds);
             }
         })
     });
@@ -23,7 +46,7 @@ export default (props: Props & ButtonProps) => {
     const pick = useCallback(async () => {
         const file = await pickFile();
         const text = await getFileText(file);
-        console.log(text);
+        const opml = await parseOpml(text);
     }, []);
 
     return <Button
