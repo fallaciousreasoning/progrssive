1b9346c8175553a1717c3ba310872727f47fda01
Jay Harris <jay.harris@outlook.co.nz>
2020-07-07 18:50:38 +1200

    Remove marker button colors

--- src/MarkerButton.tsx
+++ src/MarkerButton.tsx
@@ -1,33 +1,21 @@
 import { IconButton, makeStyles } from '@material-ui/core';
-import { RemoveRedEye } from '@material-ui/icons';
+import { Visibility, VisibilityOff } from '@material-ui/icons';
 import * as React from 'react';
 import { useCallback } from 'react';
 import { setUnread } from './actions/marker';
 import { useEntry } from './hooks/entry';
-
-const useStyles = makeStyles({
-    on: {
-        color: 'white !important'
-    },
-    off: {
-        color: 'gray !important'
-    }
-})
 
 interface MarkerButtonProps {
     entryId: string;
 }
 
 export const EntryReadButton = (props: MarkerButtonProps) => {
-    const styles = useStyles(undefined);
     const entry = useEntry(props.entryId);
     const toggleRead = useCallback(() => {
         setUnread(entry, !entry.unread);
     }, [entry]);
 
-    return <IconButton
-        className={entry && entry.unread ? styles.on : styles.off}
-        onClick={toggleRead}>
-        <RemoveRedEye/>
+    return <IconButton onClick={toggleRead}>
+        {entry.unread ? <Visibility /> : <VisibilityOff />}
     </IconButton>;
 }
