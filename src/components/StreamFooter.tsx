bfcedb8ac1e1873dcf1e8bec73677c66c3b61754
Jay Harris <jay.harris@outlook.co.nz>
2020-07-07 14:44:01 +1200

    Animate in the StreamFoot to reduce jitter

--- src/components/StreamFooter.tsx
+++ src/components/StreamFooter.tsx
@@ -5,6 +5,7 @@ import { Button, CircularProgress, Typography, makeStyles } from '@material-ui/c
 import { updateStreams } from '../actions/stream'
 import { useIsPhone } from '../hooks/responsive'
 import { useStore } from '../hooks/store'
+import { motion, AnimatePresence } from 'framer-motion'
 
 interface Props {
     unreadOnly: boolean;
@@ -16,6 +17,20 @@ const useStyles = makeStyles(theme => ({
     }
 }))
 
+const rootAnimation = {
+    initial: {
+        opacity: 0
+    },
+    animate: {
+        opacity: 1
+    }
+};
+
+const transition = {
+    duration: 1,
+    delay: 0.2
+};
+
 export default (props: Props) => {
     const styles = useStyles();
     const isPhone = useIsPhone();
@@ -23,20 +38,29 @@ export default (props: Props) => {
     const hasSubscriptions = !!store.subscriptions.length;
     const loading = !!store.updating.stream;
 
-    return <StackPanel direction="column-reverse" center>
-        <StackPanel direction={isPhone ? 'column' : 'row'}>
-            {props.unreadOnly && hasSubscriptions && <LinkButton fullWidth href="?showUnread" variant="contained" color="secondary">
-                Show Unread
-      </LinkButton>}
-            <LinkButton fullWidth href="/subscriptions?query=" variant="contained" color="secondary">
-                Add Subscriptions
-      </LinkButton>
-            {hasSubscriptions && <Button fullWidth disabled={loading} variant="contained" color="secondary" onClick={() => updateStreams(store.stream.id)}>
-                {loading && <CircularProgress size={16} className={styles.footerLoader} />} Refresh
-      </Button>}
-        </StackPanel>
-        <Typography variant='h3' align='center'>
-            {hasSubscriptions ? "That's everything!" : "You don't have any subscriptions"}
-        </Typography>
-    </StackPanel>
+    return <AnimatePresence>
+        <motion.div
+            variants={rootAnimation}
+            initial="initial"
+            animate="animate"
+            transition={transition}
+        >
+            <StackPanel direction="column-reverse" center>
+                <StackPanel direction={isPhone ? 'column' : 'row'}>
+                    {props.unreadOnly && hasSubscriptions && <LinkButton fullWidth href="?showUnread" variant="contained" color="secondary">
+                        Show Unread
+                </LinkButton>}
+                    <LinkButton fullWidth href="/subscriptions?query=" variant="contained" color="secondary">
+                        Add Subscriptions
+                </LinkButton>
+                    {hasSubscriptions && <Button fullWidth disabled={loading} variant="contained" color="secondary" onClick={() => updateStreams(store.stream.id)}>
+                        {loading && <CircularProgress size={16} className={styles.footerLoader} />} Refresh
+                </Button>}
+                </StackPanel>
+                <Typography variant='h3' align='center'>
+                    {hasSubscriptions ? "That's everything!" : "You don't have any subscriptions"}
+                </Typography>
+            </StackPanel>
+        </motion.div>
+    </AnimatePresence>
 }
