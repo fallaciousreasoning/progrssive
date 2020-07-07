2d06259a86b85173d19a8156b37eb3e70897ed56
Jay Harris <jay.harris@outlook.co.nz>
2020-07-07 13:22:03 +1200

    Remove call to setState in render

--- src/hooks/useWhenChanged.ts
+++ src/hooks/useWhenChanged.ts
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useOnMount } from "./lifeCycle";
 
 export default <T>(func: () => T, watch: any[], onInitial = true) => {
@@ -11,26 +11,28 @@ export default <T>(func: () => T, watch: any[], onInitial = true) => {
             func();
     });
 
-    if (!prev)
-        return;
+    useEffect(() => {
+        if (!prev)
+            return;
 
-    // The length of the arrays should always match.
-    if (watch.length !== prev.length) {
-        throw new Error(`Length of watch array changed!`);
-    }
-
-    // Determine whether any item in the array has changed.
-    let changed = false;
-    for (let i = 0; i < watch.length; ++i) {
-        if (watch[i] !== prev[i]) {
-            changed = true;
-            break;
+        // The length of the arrays should always match.
+        if (watch.length !== prev.length) {
+            throw new Error(`Length of watch array changed!`);
         }
-    }
 
-    // If something changed, invoke func.
-    if (changed) {
-        setPrev(watch);
-        func();
-    }
+        // Determine whether any item in the array has changed.
+        let changed = false;
+        for (let i = 0; i < watch.length; ++i) {
+            if (watch[i] !== prev[i]) {
+                changed = true;
+                break;
+            }
+        }
+    
+        // If something changed, invoke func.
+        if (changed) {
+            setPrev(watch);
+            func();
+        }
+    }, [prev, watch, func]);
 }
