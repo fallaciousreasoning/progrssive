07c15426bda84edaeaff8024ac476a41eb4a23ea
Jay Harris <jay.harris@outlook.co.nz>
2020-07-10 17:23:54 +1200

    Log statements based on guesses about what's causing the freeze

--- src/components/ProgressRing.tsx
+++ src/components/ProgressRing.tsx
@@ -59,6 +59,7 @@ export default (props: Props) => {
             scale = 1;
 
         setTextScale(scale);
+        console.log("Was it me?");
     }, [props.text, textScale]);
 
     return <div className={styles.root}>
