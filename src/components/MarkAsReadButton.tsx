717b849a5d0efe588ed89c1c717dd3946f7ebbee
Jay Harris <jay.harris@outlook.co.nz>
2020-07-12 17:36:20 +1200

    Better feedback on read/unread status

--- src/components/MarkAsReadButton.tsx
+++ src/components/MarkAsReadButton.tsx
@@ -1,5 +1,5 @@
-import { IconButton, MenuItem } from '@material-ui/core';
-import React from 'react';
+import { IconButton, MenuItem, CircularProgress } from '@material-ui/core';
+import React, { useState, useCallback } from 'react';
 import { markStreamAs } from '../services/store';
 import ProgressRing from './ProgressRing';
 import ToggleMenu from './ToggleMenu';
@@ -8,16 +8,29 @@ export default (props: {
     progress: number,
     text?: string,
 }) => {
-    return <ToggleMenu trigger={<IconButton size="small">
-        <ProgressRing key="progress"
-            percent={props.progress}
-            text={props.text} />
-    </IconButton>}>
-        <MenuItem onClick={() => markStreamAs('read')}>
-            Mark Stream as Read
+    const [isLoading, setIsLoading] = useState(false);
+    const markStream = useCallback(async e => {
+        const as = e.target.getAttribute('value');
+        setIsLoading(true);
+
+        await markStreamAs(as);
+
+        setIsLoading(false);
+        window.snackHelper.enqueueSnackbar(`Marked stream as ${as}`);
+    }, []);
+
+    return isLoading
+        ? <CircularProgress size={24} color="secondary"/>
+        : <ToggleMenu trigger={<IconButton size="small">
+            <ProgressRing key="progress"
+                percent={props.progress}
+                text={props.text} />
+        </IconButton>}>
+            <MenuItem value='read' onClick={markStream}>
+                Mark Stream as Read
         </MenuItem>
-        <MenuItem onClick={() => markStreamAs('unread')}>
-            Mark Stream as Unread
+            <MenuItem value="unread" onClick={markStream}>
+                Mark Stream as Unread
         </MenuItem>
-    </ToggleMenu>;
+        </ToggleMenu>;
 }
