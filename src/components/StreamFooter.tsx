8218ad643548ca5f8956be42f2ae3955a8660e22
Jay Harris <jay.harris@outlook.co.nz>
2020-07-13 10:52:36 +1200

    Stop footer flickering

--- src/components/StreamFooter.tsx
+++ src/components/StreamFooter.tsx
@@ -2,11 +2,10 @@ import Button from '@material-ui/core/Button';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
-import { AnimatePresence, motion } from 'framer-motion';
 import React from 'react';
 import { updateStreams } from '../actions/stream';
 import { useIsPhone } from '../hooks/responsive';
-import { useStore, getStreamUpdating } from '../hooks/store';
+import { getStreamUpdating, useStore } from '../hooks/store';
 import LinkButton from './LinkButton';
 import StackPanel from './StackPanel';
 
@@ -26,15 +25,17 @@ const useStyles = makeStyles(theme => ({
 
 const rootAnimation = {
     initial: {
-        opacity: 0
+        opacity: 0,
+        scale: 0
     },
-    animate: {
-        opacity: 1
+    in: {
+        opacity: 1,
+        scale: 1
     }
 };
 
 const transition = {
-    duration: 1,
+    duration: 0.2,
     delay: 0.2
 };
 
@@ -45,30 +46,28 @@ export default (props: Props) => {
     const hasSubscriptions = !!store.subscriptions.length;
     const loading = !!getStreamUpdating(store.stream.id);
 
-    return <AnimatePresence>
-        <motion.div
-            variants={rootAnimation}
-            initial="initial"
-            animate="animate"
-            transition={transition}
-            key="footer"
-        >
-            <StackPanel direction="column-reverse" center key={`${store.stream.id}?${store.stream.unreadOnly ? "showUnread" : ""}`}>
-                <StackPanel direction={isPhone ? 'column' : 'row'} key="buttons">
-                    {props.unreadOnly && hasSubscriptions && <LinkButton fullWidth href="?showUnread" variant="contained" color="secondary" key="showUnread">
-                        Show Read
+    return <StackPanel
+        direction="column"
+        justifyContent="start"
+        alignItems="center"
+        variants={rootAnimation}
+        transition={transition}
+        key={`${store.stream.id}?${store.stream.unreadOnly ? "showUnread" : ""}`}>
+        <Typography className={styles.text} variant='h3' align='center' key="message">
+            {hasSubscriptions
+                ? "That's everything!"
+                : "You don't have any subscriptions"}
+        </Typography>
+        <StackPanel direction={isPhone ? 'column' : 'row'} key="buttons">
+            {props.unreadOnly && hasSubscriptions && <LinkButton fullWidth href="?showUnread" variant="contained" color="secondary" key="showUnread">
+                Show Read
                 </LinkButton>}
-                <LinkButton fullWidth href="/subscriptions?query=" variant="contained" color="secondary" key="addSubscriptions">
-                        Add Subscriptions
+            <LinkButton fullWidth href="/subscriptions?query=" variant="contained" color="secondary" key="addSubscriptions">
+                Add Subscriptions
                 </LinkButton>
-                {hasSubscriptions && <Button fullWidth disabled={loading} variant="contained" color="secondary" key="refresh" onClick={() => updateStreams(store.stream.id)}>
-                        {loading && <CircularProgress size={16} className={styles.footerLoader} />} Refresh
+            {hasSubscriptions && <Button fullWidth disabled={loading} variant="contained" color="secondary" key="refresh" onClick={() => updateStreams(store.stream.id)}>
+                {loading && <CircularProgress size={16} className={styles.footerLoader} />} Refresh
                 </Button>}
-                </StackPanel>
-                <Typography className={styles.text} variant='h3' align='center' key="message">
-                    {hasSubscriptions ? "That's everything!" : "You don't have any subscriptions"}
-                </Typography>
-            </StackPanel>
-        </motion.div>
-    </AnimatePresence>
+        </StackPanel>
+    </StackPanel>
 }
