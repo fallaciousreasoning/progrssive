3456d514f195473057f582582ba63a21ad1209bd
Jay Harris <jay.harris@outlook.co.nz>
2020-09-15 11:15:06 +1200

    Move mobilizer to microservice.

--- src/services/mobilize.ts
+++ src/services/mobilize.ts
@@ -1,26 +1,8 @@
-import { getPageText } from "../utils/fetch"
 import { EntryContent } from "../model/entry";
 
+const MOBILIZER_URL = `https://mobilize.now.sh/api/mobilize/`
+
 export default async (url: string): Promise<EntryContent> => {
-    const text = await getPageText(url);
-    if (!text)
-        return;
-
-    // Import these libraries on demand. They're big, and we only
-    // use them here.
-    const [Readability, DOMPurify] = (await Promise.all([
-        import("../third_party/Readability"),
-        import("dompurify")]))
-        .map(i => i.default);
-    const document = new DOMParser()
-        .parseFromString(text, 'text/html');
-
-    const readable = new Readability(document)
-        .parse();
-
-    const content = DOMPurify.sanitize(readable.content);
-    return {
-        content,
-        direction: readable.dir
-    };
+    const response = await fetch(`${MOBILIZER_URL}${encodeURIComponent(url)}`);
+    return await response.json();
 }
