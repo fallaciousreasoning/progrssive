ef8dc0ce64f87186299a3cc2d29b746154d3d58f
Jay Harris <jay.harris@outlook.co.nz>
2020-09-15 16:56:21 +1200

    Import organising

--- src/components/EntryCard.tsx
+++ src/components/EntryCard.tsx
@@ -1,13 +1,13 @@
-import { makeStyles } from '@material-ui/core/styles'
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardMedia from '@material-ui/core/CardMedia';
 import Paper from '@material-ui/core/Paper';
+import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
-import React, { useState, useCallback, useEffect } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { Entry } from "../model/entry";
-import { getEntryByline, getEntryContent, getEntrySummary, getEntryVisualUrl } from "../services/entry";
+import { getEntryByline, getEntryContent, getEntryVisualUrl } from "../services/entry";
 
 const useStyles = makeStyles(theme => ({
     paper: {
