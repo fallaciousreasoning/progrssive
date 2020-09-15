db937ab3f21c0d78b22bb93016025664b6c12b38
Jay Harris <jay.harris@outlook.co.nz>
2020-09-15 15:34:54 +1200

    Starts work on routine for cleaning up entries

---
+++ src/utils/time.ts
@@ -0,0 +1,5 @@
+export const second = 1000 // milliseconds/second
+export const minute = second * 60 // seconds/minute
+export const hour = minute * 60 // minutes/hour
+export const day = hour * 24 // hours/day
+export const week = day * 7 // days/week
--- src/worker/worker.ts
+++ src/worker/worker.ts
@@ -1,3 +1,18 @@
+import { Entry } from "../model/entry";
+import { round } from "../utils/math";
+import { week } from "../utils/time";
+
+const shouldDelete = (now: number, entry: Entry) => {
+    // Don't delete unread articles.
+    if (!entry.readTime || entry.unread)
+        return false;
+
+    const timeSinceRead = now - entry.readTime;
+
+    // Entries older than one week should be deleted.
+    return timeSinceRead > week;
+}
+ 
 export const runEntryCleanup = async () => {
     const start = performance.now();
 
@@ -8,6 +23,15 @@ export const runEntryCleanup = async () => {
 
     const entries = await db.entries.toArray();
     console.log(`Have ${entries.length} entries!`);
+
+    const now = Date.now();
+    const oldEntries = entries.filter(e => shouldDelete(now, e as any));
+    console.log(`Found ${oldEntries.length} entries to delete`);
+
+    const deleteIds = oldEntries.map(e => e.id);
+    // Uncomment this to actually delete entries.
+    // await db.entries.bulkDelete(deleteIds);
+
     const end = performance.now();
-    console.log(`Took ${(end - start)}ms to load entries`);
+    console.log(`Took ${round(end - start)}ms to cleanup entries`);
 }
