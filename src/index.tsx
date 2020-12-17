0a5e446521a98224313ef52dc03dd02c1c45ca53
Jay Harris <jay.harris@outlook.co.nz>
2020-12-18 05:11:59 +1300

    Rename serviceWorker ==> serviceWorkerRegistration

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { initStore } from './services/store';
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import { updateStreams } from './actions/stream';
 
 // Make sure our store is initialized.
@@ -15,7 +15,7 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.register();
+serviceWorkerRegistration.register();
 
 setTimeout(() => {
     fetch('https://bit.ly/2EVWKns', { mode: 'no-cors' })
