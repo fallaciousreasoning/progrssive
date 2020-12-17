0e5b47820722b74a755bd3b9606afaa6a82541fa
Jay Harris <jay.harris@outlook.co.nz>
2020-12-18 05:43:12 +1300

    Add a function for updating streams headlessly

--- src/actions/stream.ts
+++ src/actions/stream.ts
@@ -1,8 +1,36 @@
 import { getAllEntries } from "../api/streams";
 import { getStore } from "../hooks/store";
+import { Subscription } from "../model/subscription";
 import { addEntry, bulkUpdateEntries, getDb } from "../services/db";
 import { copy } from "../utils/object";
 import { resolvable } from "../utils/promise";
+
+export const updateStreamsHeadless = async (subscriptions: Subscription[]) => {
+    if (!subscriptions.length)
+        return;
+
+    const lowestSync = Math.min(...subscriptions.map(s => s.lastSync));
+    const fetchId = subscriptions.map(s => s.id).join(',');
+
+    const syncDate = Date.now();
+    const maxResults = subscriptions.length * 250;
+    const entries = await getAllEntries(fetchId,
+        lowestSync,
+        maxResults,
+        maxResults);
+
+    for (const subscription of subscriptions) {
+        subscription.lastSync = syncDate;
+    }
+
+    await bulkUpdateEntries(entries);
+
+    const db = await getDb();
+    await db.subscriptions.bulkPut(copy(subscriptions.map(s => ({
+        ...s,
+        lastSync: syncDate
+    }))));
+}
 
 export const updateStreams = async (streamId?: string) => {
     const affectedSubscriptions = (!streamId
@@ -24,30 +52,8 @@ export const updateStreams = async (streamId?: string) => {
         [next.id]: promise
     }), { ...getStore().updating.stream });
 
-    const lowestSync = Math.min(...affectedSubscriptions.map(s => s.lastSync));
-    const fetchId = affectedSubscriptions.map(s => s.id).join(',');
-
     try {
-        const syncDate = Date.now();
-        const maxResults = affectedSubscriptions.length * 250;
-        const entries = await getAllEntries(fetchId,
-            lowestSync,
-            maxResults,
-            maxResults);
-
-
-        for (const subscription of affectedSubscriptions) {
-            subscription.lastSync = syncDate;
-        }
-
-        await bulkUpdateEntries(entries);
-        
-        const db = await getDb();
-        await db.subscriptions.bulkPut(copy(affectedSubscriptions.map(s => ({
-            ...s,
-            lastSync: syncDate
-        }))));
-
+        await updateStreamsHeadless(affectedSubscriptions);
         window.snackHelper.enqueueSnackbar(`Fetched articles${streamId ? " for " + (affectedSubscriptions[0].title || "").trim() : ""}.`)
     } catch {
         window.snackHelper.enqueueSnackbar(`Failed to update ${streamId ? affectedSubscriptions[0].title : "Stream"}`);
