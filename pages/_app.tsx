26e7143b45feeeedc2eddad1ae808292e91b7fa2
Jay Harris <jay.harris@outlook.co.nz>
2021-06-18 11:15:39 +0200

    Migrate index.html to _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,6 +15,7 @@ import { buildTheme } from '../styles/theme';
 import { CollectProps } from '../types/RecollectStore';
 import '../types/Window';
 import WebWorker from '../worker';
+import Head from 'next/head'
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -33,6 +34,16 @@ const ProgrssiveApp = collect(({ Component, pageProps, store }: AppProps & Colle
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [store.settings, prefersDark]);
   return <MuiThemeProvider theme={theme}>
+    <Head>
+      <meta charSet="utf-8" />
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+      <meta name="theme-color" content="#4caf50" />
+      <link rel="shortcut icon" href="/icon-green.png" />
+      <link rel="manifest" href="/manifest.json" />
+      <title>Progrssive Reader</title>
+    </Head>
     <LazySnackbarProvider>
       <div className={styles.root}>
         <AppBar>
