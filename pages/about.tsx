6f204e598581e43b3d4d660011ea387b75949729
Jay Harris <jay.harris@outlook.co.nz>
2021-06-18 11:17:40 +0200

    Add about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,5 +1,15 @@
+import { Typography } from "@material-ui/core";
+
 export default function AboutPage() {
     return <div>
-        About
+        <Typography variant="h4">
+            About
+        </Typography>
+        <Typography gutterBottom>
+            Progrssive is an offline web app for reading RSS feeds, created by <a target="_blank" rel="noopener noreferrer" href="https://github.com/fallaciousreasoning">fallaciousreasoning</a> (take a look at <a target="_blank" rel="noopener noreferrer" href="https://github.com/fallaciousreasoning/cv/blob/master/README.md">my CV</a>).
+        </Typography>
+        <Typography>
+            The source code is available on <a target="_blank" rel="noopener noreferrer" href="https://github.com/fallaciousreasoning/progrssive">Github</a>.
+        </Typography>
     </div>
 }
