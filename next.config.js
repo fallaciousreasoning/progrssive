cc45febc6759f4b1e3799cf82d77a49f9762904b
Jay Harris <jay.harris@outlook.co.nz>
2021-07-09 13:41:07 +0200

    Add bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+
 module.exports = {
     eslint: {
         ignoreDuringBuilds: true
+    },
+    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+        if (process.env.ANALYZE) {
+            config.plugins.push(
+                new BundleAnalyzerPlugin({
+                    analyzerMode: 'server',
+                    analyzerPort: isServer ? 8888 : 8889,
+                    openAnalyzer: true,
+                })
+            )
+        }
+        return config
     },
     async redirects() {
         return [
