1728da93e1a4d42e55301bd13b92559ad144c052
Jay Harris <jay.harris@outlook.co.nz>
2021-07-13 15:26:01 +0200

    More removal of material ui

--- components/Centre.tsx
+++ components/Centre.tsx
@@ -1,17 +1,7 @@
-import { makeStyles } from "@material-ui/core";
 import * as React from 'react';
 
-const useStyles = makeStyles({
-    root: {
-        display: 'flex',
-        justifyContent: 'center'
-    }
-});
-
 export default function Centre(props: { children: React.ReactNode, className?: string }) {
-    const styles = useStyles();
-
-    return <div className={`${props.className} ${styles.root}`}>
+    return <div className={`${props.className} flex justify-center`}>
         {props.children}
     </div>
 }
--- components/EntryCard.tsx
+++ components/EntryCard.tsx
@@ -1,13 +1,6 @@
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardMedia from '@material-ui/core/CardMedia';
-import Paper from '@material-ui/core/Paper';
-import { makeStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
 import React, { memo, useCallback, useEffect, useMemo, useState } from 'react';
 import { Entry } from "../model/entry";
-import { getEntryByline, getEntryContent, getEntrySummary, getEntryVisualUrl } from "../services/entry";
+import { getEntryByline, getEntrySummary, getEntryVisualUrl } from "../services/entry";
 
 const maxLines = {
     maxLines: 4
