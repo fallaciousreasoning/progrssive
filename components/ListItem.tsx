1bacf2944b971b5386d3ee65dc52f610119a2ef3
Jay Harris <jay.harris@outlook.co.nz>
2021-08-04 18:09:12 +0200

    Update holdout menu items

--- components/ListItem.tsx
+++ components/ListItem.tsx
@@ -2,11 +2,12 @@ export interface ListItemProps {
     children?: React.ReactNode;
     secondary?: string;
     primary?: string;
+    onTextClick?: (e: React.MouseEvent) => void;
 }
 
 export default function ListItem(props: ListItemProps) {
     return <li className="px-4 py-2 flex flex-row">
-        <div className="flex flex-col flex-1">
+        <div className="flex flex-col flex-1" onClick={props.onTextClick}>
             {props.primary && <span className="text-lg">{props.primary}</span>}
             {props.secondary && <span className="font-light">{props.secondary}</span>}
         </div>
--- components/ListOptionToggle.tsx
+++ components/ListOptionToggle.tsx
@@ -1,6 +1,6 @@
-import { ListItem, ListItemText } from "@material-ui/core";
 import React, { useCallback } from 'react';
 import Toggle from 'components/Toggle';
+import ListItem from 'components/ListItem'
 
 export default function ListOptionToggle(
     props: {
@@ -15,8 +15,7 @@ export default function ListOptionToggle(
         e.target['name'] = props.name;
         props.onChange(e as any, !props.value)
     }, [props.name, props.onChange, props.value]);
-    return <ListItem>
-        <ListItemText onClick={toggleManual} primary={props.primaryText} secondary={props.secondaryText} />
+    return <ListItem primary={props.primaryText} secondary={props.secondaryText} onTextClick={toggleManual}>
         <Toggle checked={props.value} onChange={props.onChange} name={props.name} /> 
     </ListItem>;
 }
