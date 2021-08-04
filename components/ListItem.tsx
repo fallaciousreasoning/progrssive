b8c6a11d55120f22586b25a3d3032f85824be46d
Jay Harris <jay.harris@outlook.co.nz>
2021-08-04 18:12:33 +0200

    Fix sizing on small screens

--- components/ListItem.tsx
+++ components/ListItem.tsx
@@ -6,7 +6,7 @@ export interface ListItemProps {
 }
 
 export default function ListItem(props: ListItemProps) {
-    return <li className="px-4 py-2 flex flex-row">
+    return <li className="px-4 py-2 flex flex-row items-start">
         <div className="flex flex-col flex-1" onClick={props.onTextClick}>
             {props.primary && <span className="text-lg">{props.primary}</span>}
             {props.secondary && <span className="font-light">{props.secondary}</span>}
