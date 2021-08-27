855c5a4c51c1587fd11fd81ba12488c588bdfb7c
Jay Harris <jay.harris@outlook.co.nz>
2021-08-27 16:12:13 +0200

    Remove drawer

--- components/AppDrawer.tsx
+++ components/AppDrawer.tsx
@@ -1,5 +1,5 @@
-import Drawer from '@material-ui/core/Drawer';
 import React, { useCallback, useMemo, useState } from 'react';
+import ReactDOM from 'react-dom';
 import useInstallPrompt from '../hooks/useInstallPrompt';
 import About from '../icons/about.svg';
 import Articles from '../icons/articles.svg';
@@ -7,6 +7,15 @@ import Install from '../icons/install.svg';
 import Settings from '../icons/settings.svg';
 import Subscriptions from '../icons/subscriptions.svg';
 import ListItemButton from './ListItemButton';
+
+function Drawer(props: { isOpen: boolean, onClose: () => void, children: React.ReactNode }) {
+    return ReactDOM.createPortal(<>
+        {props.isOpen && <div className="fixed top-0 bottom-0 left-0 right-0 opacity-30 bg-foreground z-10" onClick={props.onClose} />}
+        <div className={`fixed z-20 top-0 bottom-0 left-0 bg-background transform transition-transform ${props.isOpen ? 'translate-x-0' : '-translate-x-full'}`}>
+            {props.children}
+        </div>
+    </>, document.body);
+}
 
 const iconProps = { className: "opacity-50", width: 24, height: 24 };
 export default function AppDrawer(props: { trigger: JSX.Element }) {
@@ -23,7 +32,7 @@ export default function AppDrawer(props: { trigger: JSX.Element }) {
 
     return <div>
         {React.cloneElement(props.trigger, triggerProps)}
-        <Drawer open={open} onClose={close}>
+        <Drawer isOpen={open} onClose={close}>
             <div className="m-2 w-64">
                 <h5 className="text-xl ml-2">
                     Progrssive Reader
@@ -33,7 +42,7 @@ export default function AppDrawer(props: { trigger: JSX.Element }) {
                     <ListItemButton icon={<Subscriptions {...iconProps} />} text="Subscriptions" href="/subscriptions" />
                     <ListItemButton icon={<Settings {...iconProps} />} text="Settings" href="/settings" />
                     <ListItemButton icon={<About {...iconProps} />} text="About" href="/about" />
-                    {installPrompt && <ListItemButton onClick={installPrompt} icon={Install} text="Install"/>}
+                    {installPrompt && <ListItemButton onClick={installPrompt} icon={Install} text="Install" />}
                 </ul>
             </div>
         </Drawer>
