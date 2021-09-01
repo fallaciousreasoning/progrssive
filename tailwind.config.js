a23981b8f1e6a22a7682dd9cccd04401dfcb4a03
Jay Harris <jay.harris@outlook.co.nz>
2021-09-01 15:53:25 +0200

    Fix loading spinner

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      fill: {
+        background: 'var(--background-color)'
+      },
       colors: {
         primary: {
           DEFAULT: 'var(--primary-color)',
