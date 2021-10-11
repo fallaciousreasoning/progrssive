15f47687ebcc524c7a9dbde0b4f72915bca9fe39
Jay Harris <jay.harris@outlook.co.nz>
2021-10-11 16:23:01 +0200

    Fix functions

--- pages/api/stream/[streamId].ts
+++ pages/api/stream/[streamId].ts
@@ -1,7 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 
 export default function(request: NextApiRequest, response: NextApiResponse) {
-    response.status(200);
-    response.setHeader('Content-Type', 'application/json');
-    response.write(JSON.stringify({ streamId: request.query.streamId }));
+    response.send({ streamId: request.query.streamId });
 }
