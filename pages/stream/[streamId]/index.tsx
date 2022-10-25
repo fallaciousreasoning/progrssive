aea794e6209400caeb346fdea4bef7434a510613
Jay Harris <jay.harris@outlook.co.nz>
2022-10-25 20:52:12 +1300

    Name export

--- pages/stream/[streamId]/index.tsx
+++ pages/stream/[streamId]/index.tsx
@@ -3,7 +3,7 @@ import React, { Suspense } from "react";
 
 const Stream = React.lazy(() => import('components/Stream'))
 
-export default function () {
+export default function StreamPage() {
   return <Suspense fallback={<div className="w-full flex justify-center">
     <LoadingSpinner />
   </div>}>
